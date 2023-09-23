ac5bbe812a4b7d10640bc76c388cfd20c3191ac0
MarKawaii <101842633+MarKawaii@users.noreply.github.com>
2023-09-23 18:16:01 -0300

    feat: validar formulario
    Se valida el formulario de inicio de sesion

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
+import { AlertController } from '@ionic/angular';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -9,16 +11,40 @@ import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms'
 export class LoginPage implements OnInit {
 
   formularioLogin: FormGroup;
+  mostrarContrasena: boolean = false;
 
-  constructor(public fb: FormBuilder) { 
+  constructor(private router: Router, public fb: FormBuilder, public alertController: AlertController,) { 
     // Corrección: Debes pasar un objeto con las claves y FormControl
     this.formularioLogin = this.fb.group({
-      'email': new FormControl("", Validators.required),
-      'password': new FormControl("", Validators.required)
+      'email': new FormControl("", [Validators.required, Validators.email]),
+      'password': new FormControl("", [Validators.required, Validators.minLength(5)])
     });
   }
-
+  
   ngOnInit() {
+  }
+
+  toggleContrasena(): void {
+    this.mostrarContrasena = !this.mostrarContrasena;
+  }
+
+  async iniciarSesion() {
+    console.log('Formulario válido:', this.formularioLogin.valid); // Añade esto
+    
+    // Si el formulario esta completo se intenta iniciar sesion. con la informacion del usuario
+    if (this.formularioLogin.valid) {
+      this.router.navigate(['/home']);
+    } else {
+      const alert = await this.alertController.create({
+        header: 'Alerta',
+        message: 'Por favor, llena todos los campos corectamente',
+        buttons: [{
+            text: 'OK',
+            cssClass: 'primary'
+        }]
+      });
+      await alert.present();
+    }
   }
 
 }
--- src/app/pages/registro/registro.page.ts
+++ src/app/pages/registro/registro.page.ts
@@ -1,8 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators, FormBuilder} from '@angular/forms';
+import {
+  FormGroup,
+  FormControl,
+  Validators,
+  FormBuilder,
+} from '@angular/forms';
 import { AlertController } from '@ionic/angular';
 import { Router } from '@angular/router';
-
 
 @Component({
   selector: 'app-registro',
@@ -10,18 +14,29 @@ import { Router } from '@angular/router';
   styleUrls: ['./registro.page.scss'],
 })
 export class RegistroPage implements OnInit {
-
   formularioRegistro: FormGroup;
   mostrarContrasena: boolean = false;
 
-  constructor(private router: Router, public fb: FormBuilder, public alertController: AlertController, ) {
+  constructor(
+    private router: Router,
+    public fb: FormBuilder,
+    public alertController: AlertController
+  ) {
     this.formularioRegistro = this.fb.group({
-      'firsName': new FormControl("", [Validators.required, Validators.minLength(3)]),
-      'lastName': new FormControl("", [Validators.required, Validators.minLength(3)]),
-      'email': new FormControl("", [Validators.required, Validators.email]),
-      'password': new FormControl("", [Validators.required, Validators.minLength(5)])
+      firsName: new FormControl('', [
+        Validators.required,
+        Validators.minLength(3),
+      ]),
+      lastName: new FormControl('', [
+        Validators.required,
+        Validators.minLength(3),
+      ]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(5),
+      ]),
     });
-    
   }
 
   ngOnInit() {}
@@ -30,23 +45,24 @@ export class RegistroPage implements OnInit {
     this.mostrarContrasena = !this.mostrarContrasena;
   }
 
+  // SI EL FORMULARIO ESTA COMPLETO SE INTENTA CREAR EL USUARIO CON LA INFORMACION INGRESADA
   async crearCuenta() {
     console.log('Formulario válido:', this.formularioRegistro.valid); // Añade esto
-  
+
     if (this.formularioRegistro.valid) {
-      this.router.navigate(['/']);
+      this.router.navigate(['/home']);
     } else {
       const alert = await this.alertController.create({
-        header: 'Error',
+        header: 'Alerta',
         message: 'Por favor, llena todos los campos corectamente',
-        buttons: [{
+        buttons: [
+          {
             text: 'OK',
-            cssClass: 'primary'
-        }]
+            cssClass: 'primary',
+          },
+        ],
       });
       await alert.present();
     }
   }
-  
-
 }
