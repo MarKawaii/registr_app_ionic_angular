124460189b89ff020d8382ede9f753db8191b82c
MarKawaii <101842633+MarKawaii@users.noreply.github.com>
2023-09-24 02:54:33 -0300

    feat: views
    Se modifican las vistas

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef } from '@angular/core';
+
 import { MenuController } from '@ionic/angular';
 import { ApiService } from 'src/app/services/api.service';
 import { Feriados } from '../interfaces/interfaces';
@@ -8,40 +10,33 @@ import { Feriados } from '../interfaces/interfaces';
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
 })
-
 export class HomePage implements OnInit {
+  // Añadir OnDestroy
   feriados: Feriados[] = [];
   filterActive: boolean = true;
   loading: boolean = true;
+  currentTime: string = new Date().toISOString();
 
   constructor(
     private menuController: MenuController,
-    private apiService: ApiService
+    private apiService: ApiService,
+    private cdRef: ChangeDetectorRef // Añadir esto
   ) {}
 
   ngOnInit() {
     this.cargarFeriados();
   }
 
-  /**
-   * Carga los feriados basados en los ajustes de filtro y la fecha actual.
-   */
   cargarFeriados() {
     const fechaActual = new Date();
 
     this.apiService.getTopHeadLines().subscribe((resp) => {
       const feriadosProximos = this.filtrarFeriados(resp.data, fechaActual);
       this.loading = false;
-
       this.feriados = [...feriadosProximos];
     });
   }
 
-  /**
-   * Filtra los feriados basándose en la fecha actual y los ajustes de filtro.
-   * @param listaFeriados - Lista de todos los feriados.
-   * @param fechaActual - La fecha actual.
-   */
   filtrarFeriados(listaFeriados: Feriados[], fechaActual: Date): Feriados[] {
     let feriadosFiltrados = listaFeriados.filter((feriado) => {
       const fechaFeriado = new Date(feriado.date);
@@ -49,7 +44,9 @@ export class HomePage implements OnInit {
     });
 
     if (this.filterActive) {
-      feriadosFiltrados = feriadosFiltrados.filter(feriado => feriado.inalienable === true);
+      feriadosFiltrados = feriadosFiltrados.filter(
+        (feriado) => feriado.inalienable === true
+      );
     }
 
     feriadosFiltrados.forEach((feriado) => {
@@ -65,10 +62,6 @@ export class HomePage implements OnInit {
     return feriadosFiltrados;
   }
 
-  /**
-   * Formatea una fecha de yyyy-mm-dd a dd-mm-yyyy.
-   * @param fechaEntrada - Cadena de fecha en formato yyyy-mm-dd.
-   */
   formatearFecha(fechaEntrada: string): string {
     const fecha = new Date(fechaEntrada);
     const dia = fecha.getDate().toString().padStart(2, '0');
@@ -77,9 +70,6 @@ export class HomePage implements OnInit {
     return `${dia}-${mes}-${ano}`;
   }
 
-  /**
-   * Cambia el ajuste de filtro y vuelve a cargar los feriados.
-   */
   cambiarFiltro() {
     this.filterActive = !this.filterActive;
     this.cargarFeriados();
